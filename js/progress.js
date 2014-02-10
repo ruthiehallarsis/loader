96fb8f49e116212febbcbb496b619138be58dbbb
ruthie borces <ruthieborces@outlook.com>
2014-02-10 12:35:08 +0800

    fdsaf

--- js/progress.js
+++ js/progress.js
@@ -286,9 +286,10 @@ var bar_perc = Math.round(100 / image_count);
 
 
 	$('#bar').animate({'width': new_b_width + '%'}, 500);
+	document.getElementById('bar').style.width = new_b_width + '%';
 	//document.getElementById('nc').style.width = percent + '%';
 	$('#nc').animate({'width': new_b_width + '%'}, 500);
-	
+	document.getElementById('nc').style.width = new_b_width + '%';
 	//checkstate(); // need for safari
 	//document.getElementById('bar').innerHTML = document.readyState;
 	
